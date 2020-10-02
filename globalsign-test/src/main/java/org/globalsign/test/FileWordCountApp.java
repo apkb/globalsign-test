9a903a691f1859586ce4c887c66b2c8f5330ee92
apkb <amanpreet.913@gmail.com>
2020-10-02 19:47:20 +0530

    added test to check reading binary file

--- globalsign-test/src/main/java/org/globalsign/test/FileWordCountApp.java
+++ globalsign-test/src/main/java/org/globalsign/test/FileWordCountApp.java
@@ -6,7 +6,7 @@ public class FileWordCountApp {
 	public WordCountPair[] getMostFreqWords(int k, String filename) {
 		Map<String, Integer> wcMap = FileProcessor.getFileWordsWithFrequency("src/main/resources/" + filename);
 		// System.out.println(wcMap);
-		System.out.println("Total words in the file =  " + wcMap.size());
+		System.out.println("Total words in the file "+filename+"=  " + wcMap.size());
 		WordCountPair[] wordcountpair = FileProcessor.getWordCountSortedByCount(k, wcMap);
 		return wordcountpair;
 
@@ -15,6 +15,7 @@ public class FileWordCountApp {
 	public static void main(String[] args) {
 		FileWordCountApp app = new FileWordCountApp();
 		WordCountPair[] wordcount = app.getMostFreqWords(20, "mobydick.txt");
+		//WordCountPair[] wordcount = app.getMostFreqWords(20, "mobydickbin.dat");
 		// Print top 20
 		for (int j = 0; j < 20; j++) {
 			System.out.println(wordcount[j].getWord() + ":" + wordcount[j].getCount());
--- globalsign-test/src/test/java/org/globalsign/test/FileWordCountAppTest.java
+++ globalsign-test/src/test/java/org/globalsign/test/FileWordCountAppTest.java
@@ -6,10 +6,22 @@ import org.junit.Test;
 
 public class FileWordCountAppTest {
 	
-	@Test
-	public void giveninputFile_when_getMostFreqWordsiscalled_thenreturn20mostfrequentwords(){
+	@Test  
+	//test to read text file and count 20 most frequent words in that file
+	public void giveninputTXTFile_when_getMostFreqWordsiscalled_thenreturn20mostfrequentwords(){
 		FileWordCountApp app = new FileWordCountApp();
 		WordCountPair[] wcPair = app.getMostFreqWords(20, "mobydick.txt");
+		assertTrue(wcPair.length==20);
+		for(int i=0;i<wcPair.length-1;i++) {
+			assertTrue(wcPair[i].getCount()>=wcPair[i+1].getCount());
+		}
+	}
+	
+	@Test  
+	//test to read binary file and count 20 most frequent words in that file
+	public void giveninputBINFile_when_getMostFreqWordsiscalled_thenreturn20mostfrequentwords(){
+		FileWordCountApp app = new FileWordCountApp();
+		WordCountPair[] wcPair = app.getMostFreqWords(20, "mobydickbin.dat");
 		assertTrue(wcPair.length==20);
 		for(int i=0;i<wcPair.length-1;i++) {
 			assertTrue(wcPair[i].getCount()>=wcPair[i+1].getCount());
